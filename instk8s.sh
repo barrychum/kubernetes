a0716fbcfd68bc16cf1a833abe8a05843291433e
barry <barrychum@gmail.com>
2021-06-26 21:54:49 +0800

    Update instk8s.sh

--- instk8s.sh
+++ instk8s.sh
@@ -41,6 +41,7 @@ apt-get install -y kubelet kubeadm kubectl
 kubeadm init
 kubectl taint nodes $(kubectl get nodes --selector=node-role.kubernetes.io/master | awk 'FNR==2{print $1}') node-role.kubernetes.io/master-
 mkdir -p $HOME/.kube
+mkdir $HOME/.kube
 cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
