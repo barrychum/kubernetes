6ada0994f2a9a27f483f86d8368e06eac5e0ef18
barry <barrychum@gmail.com>
2021-06-27 11:31:08 +0800

    init

--- instk8s.sh
+++ instk8s.sh
@@ -8,43 +8,87 @@ sed -e '/swap/ s/^#*/#/' -i /etc/fstab
 
 # install all en locales for docker just in case
 # dpkg-reconfigure locales
-sed -i -e "s/# en_HK.UTF-8 UTF-8/en_HK.UTF-8 UTF-8/g" /etc/locale.gen
+sed -i -e "s/# en/en/g" /etc/locale.gen
 locale-gen
 
 # enable bridge netfilter
-# modprobe br_netfilter
-# echo 'net.bridge.bridge-nf-call-iptables = 1' > /etc/sysctl.d/20-bridge-nf.conf
-# sysctl --system
+modprobe br_netfilter
+echo 'net.bridge.bridge-nf-call-iptables = 1' > /etc/sysctl.d/20-bridge-nf.conf
+sysctl --system
 
 apt-get update
 apt-get install sudo -y
 apt-get install -y apt-transport-https ca-certificates curl gnupg2
 
-# install docker 
-apt install docker.io -y
-# export LC_CTYPE=en_HK.UTF-8
-# export LC_ALL=en_HK.UTF-8
-systemctl start docker
-systemctl enable docker
+# install docker (using recommended systemd driver)
+mkdir /etc/docker;
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": { "max-size": "100m" },
+  "storage-driver": "overlay2"
+}
+EOF
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -;
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list;
+apt-get update;
+apt-get install -y --no-install-recommends docker-ce;
+
+# install docker (using cgroupfs, deprecated in v1.21)
+# apt install docker.io -y
+## export LC_CTYPE=en_HK.UTF-8
+## export LC_ALL=en_HK.UTF-8
+# systemctl start docker
+# systemctl enable docker
 
 # install kubernetes
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
+# next line from kubernetes.io
 ### curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 ### echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 
-kubeadm init --pod-network-cidr=10.244.0.0/16
-# kubeadm init  ## failed to start pods
 mkdir -p $HOME/.kube
+
+# kubeadm init  ## failed to start pods without pod-network param
 ## mkdir $HOME/.kube
+
+cat > $HOME/nodeinit.sh <<EOF
+#!/bin/bash
+
+set -e
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
 cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+kubectl apply -f https://raw.githubusercontent.com/stellarhub/kubernetes/main/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/stellarhub/kubernetes/main/kube-flannel-rbac.yml
+kubectl taint nodes --all node-role.kubernetes.io/master-
+EOF
 
-kubectl taint nodes $(kubectl get nodes --selector=node-role.kubernetes.io/master | awk 'FNR==2{print $1}') node-role.kubernetes.io/master-
+# need to apply control plane isolation for single node environment
+# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/
+# the following find nodes with master role only
+# echo 'kubectl taint nodes $(kubectl get nodes --selector=node-role.kubernetes.io/master | awk "FNR==2{print $1}") node-role.kubernetes.io/master-' >> $HOME/nodeinit.sh
 
+chmod +x $HOME/nodeinit.sh
+$HOME/nodeinit.sh
+
+
+cat > $HOME/nodereset.sh <<EOF
+#!/bin/bash
+
+set -e
+kubeadm reset
+rm /etc/cni/net.d/*
+rm $HOME/.kube/config
+EOF
+
+chmod +x $HOME/nodereset.sh
