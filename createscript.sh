789dd87c3249556bac96f4fc5f60ab45062575ce
barry <barrychum@gmail.com>
2021-07-05 20:44:23 +0800

    helper script

--- createscript.sh
+++ createscript.sh
@@ -33,9 +33,17 @@ chmod +x $HOME/nodereset.sh
 
 cat > $HOME/nodejoin.sh <<\EOF
 #!/bin/bash
+echo "What is the master node IP "
+read masterip
+
 scp root@$masterip:/etc/kubernetes/admin.conf $HOME/.kube/config
+execcmd=$(ssh root@$masterip kubeadm token create --print-join-command --ttl=1m)
+$execcmd
+
 kubectl apply -f https://raw.githubusercontent.com/barrychum/kubernetes/main/components.yaml
+
 kubectl label node $HOSTNAME node-role.kubernetes.io/worker=worker
 kubectl get nodes
+
 EOF
 chmod +x $HOME/nodejoin.sh
--- nodejoin.sh
+++ nodejoin.sh
@@ -3,13 +3,10 @@ echo "What is the master node IP "
 read masterip
 
 scp root@$masterip:/etc/kubernetes/admin.conf $HOME/.kube/config
+execcmd=$(ssh root@$masterip kubeadm token create --print-join-command --ttl=1m)
+$execcmd
 
 kubectl apply -f https://raw.githubusercontent.com/barrychum/kubernetes/main/components.yaml
 
-echo
-
 kubectl label node $HOSTNAME node-role.kubernetes.io/worker=worker
 kubectl get nodes
-
-echo
-
