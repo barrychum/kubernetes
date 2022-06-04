64667d94f74d48515fb8cec153bf77cc5ef037c5
barry <barrychum@gmail.com>
2022-06-05 07:37:14 +0800

    updated for v1.24

---
+++ v.1.24.instk8s.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+## disable swap for kubelet requirement (swap is alpha support after v1.22)
+swapoff -a
+sed -e '/swap/ s/^#*/#/' -i /etc/fstab
+
+##
+## https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+##
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/20-k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+###############
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": { "max-size": "100m" },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo systemctl restart docker
+
+vi /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+################
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+##
+## https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+## 
+
+######
+
+mkdir -p $HOME/.kube
+
+chmod +x createscript.sh
+
+./createscript.sh
+
+# insall kubens tool
+# apt-get install -y kubectx
+cd /tmp
+git clone https://github.com/ahmetb/kubectx
+cp kubectx/kubectx /usr/local/bin/kubectx
+cp kubectx/kubens /usr/local/bin/kubens
