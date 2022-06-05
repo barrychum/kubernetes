5e5a1469ede8a4a210bdb990c92196eb41f604a1
barry <barrychum@gmail.com>
2022-06-05 08:03:11 +0800

    updated kube-flannel.yml source url

--- createscript.sh
+++ createscript.sh
@@ -5,7 +5,7 @@ kubeadm init --pod-network-cidr=10.244.0.0/16
 cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://raw.githubusercontent.com/stellarhub/kubernetes/main/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/barrychum/kubernetes/main/kube-flannel.yml
 
 # https://kubernetes.io/docs/tasks/debug-application-cluster/resource-metrics-pipeline/
 # kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.0/components.yaml
