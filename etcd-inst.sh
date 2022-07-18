11f9a89387fee9be94a8ca58dcbcf3122f6e2997
barry <barrychum@gmail.com>
2022-07-18 22:05:25 +0800

    Update etcd-inst.sh

--- etcd-inst.sh
+++ etcd-inst.sh
@@ -1,5 +1,5 @@
-git clone http://github.com/etcd-io/etcd.git etcdsrc
-cd etcdsrc
+git clone -b v3.5.0 http://github.com/etcd-io/etcd.git 
+cd etcd/
 apt install -y golang
-./build
+./build.sh
 cp bin/* /usr/local/bin
