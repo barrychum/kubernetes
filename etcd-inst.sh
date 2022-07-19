693cf29a96282a37dd47729057f8cfae0dde6498
barry <barrychum@gmail.com>
2022-07-19 20:48:40 +0800

    Update etcd-inst.sh

--- etcd-inst.sh
+++ etcd-inst.sh
@@ -1,8 +1,16 @@
-apt install -y golang
+ETCD_VER=v3.4.19
 
-cd /tmp
-rm -rf etcd
-git clone -b v3.4.0 https://github.com/etcd-io/etcd.git 
-cd etcd
-./build
-cp bin/* /usr/local/bin
+# choose either URL
+GOOGLE_URL=https://storage.googleapis.com/etcd
+GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
+DOWNLOAD_URL=${GOOGLE_URL}
+
+rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+rm -rf /tmp/etcd-download-test && mkdir -p /tmp/etcd-download-test
+
+curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd-download-test --strip-components=1
+rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+
+/tmp/etcd-download-test/etcd --version
+/tmp/etcd-download-test/etcdctl version
